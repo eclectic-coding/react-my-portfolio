25d67e172568a2e99b643d27fe2d871ae839be4d
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-01-31 15:14:31 -0500

    Update CSS Grid layout on the Home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,23 @@ import Navbar from './Navbar/Navbar'
 const Home = () => {
   return (
     <div className="container">
-      <Navbar />
-      <main>Main content</main>
-      <footer>Footer content</footer>
+      {/*<header>Header</header>*/}
+      <nav>Navigation</nav>
+      <main>
+        <h1>Main</h1>
+        <p>
+          Vestibulum consectetur sit amet nisi ut consectetur. Praesent efficitur, nibh
+          vitae fringilla scelerisque, est neque faucibus quam, in iaculis purus libero
+          eget mauris. Curabitur et luctus sapien, ac gravida orci. Aliquam erat volutpat.
+          In hac habitasse platea dictumst. Aenean commodo, arcu a commodo efficitur,
+          libero dolor mollis turpis, non posuere orci leo eget enim. Curabitur sit amet
+          elementum orci, pulvinar dignissim urna. Morbi id ex eu ex congue laoreet.
+          Aenean tincidunt dolor justo, semper pretium libero luctus nec. Ut vulputate
+          metus accumsan leo imperdiet tincidunt. Phasellus nec rutrum dolor. Cras
+          imperdiet sollicitudin arcu, id interdum nibh fermentum in.
+        </p>
+      </main>
+      <footer>Footer</footer>
     </div>
   )
 }
